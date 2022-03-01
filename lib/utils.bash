1f4b550a963d17ff0caafa7abe144bfa2f6ff73c
Denis Lavrushko <denis.lavrushko@iits-consulting.de>
2022-03-01 17:59:40 +0100

    troubleshooting

--- lib/utils.bash
+++ lib/utils.bash
@@ -70,7 +70,7 @@ install_version() {
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
     local tool_cmd
-    chmode +x tool_cmd
+    chmod +x tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
 
